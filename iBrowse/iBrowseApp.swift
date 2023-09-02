1fb58c4d882908402728694be3406b32ad9d26bf
Kovs705 <EuKovs@gmail.com>
2023-09-02 12:47:22 +0300

    Initial Commit

---
+++ iBrowse/iBrowseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBrowseApp.swift
+//  iBrowse
+//
+//  Created by Eugene Kovs on 02.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct iBrowseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
